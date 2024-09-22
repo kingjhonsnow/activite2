e857128a2ead96945456723447e9fcb0af9930fa
ahmed ben hassen <Ahmed.BENHASSEN@esprim.tn>
2024-09-23 00:00:41 +0200

    Add files via upload

---
+++ app/racine/racine.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ListCategoriesComponentComponent } from './list-categories-component/list-categories-component.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent,
+    ListCategoriesComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RacineModule { }
